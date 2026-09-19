0c62118277c0fb9e804bbec8730e8e938c40146d
patriot612 <rm9r6tkvr8@privaterelay.appleid.com>
2026-09-19 23:51:21 +0400

    Add files via upload

---
+++ src/localization/types.ts
@@ -0,0 +1,14 @@
+export interface LocaleStrings {
+  welcome: string;
+  chooseLanguage: string;
+  languageButtons: {
+    ru: string;
+    en: string;
+    uz: string;
+  };
+  errorGeneric: string;
+  pausedNotice: string;
+  youtubeTooLong: string;
+  youtubeChooseQuality: string;
+  unsupportedLink: string;
+}
